8b6524a9aa34218a19812e44f08abc9f91238cb4
Rodrigo Yoshima <rodrigoy@gmail.com>
2010-07-01 17:29:08 -0300

    changed the dsl - demo mode to deploy and get feedback

--- application.rb
+++ application.rb
@@ -10,22 +10,22 @@ get '/' do
   
   #this is spike code    
   kanban = Kanban.new
-  kanban.add_stage('selected').stages[0].add_card('A').add_card('B')
-  kanban.add_stage('development', limit=4).
+  kanban.add_stage('selected').stages[0].add_card('A').add_card('B').add_card('R').add_card('T')
+  kanban.add_stage('development', limit=5).
     stages[1].
       add_substage('acceptance', limit=1).
         substages[0].add_card('C').add_card('Z')
   kanban.
     stages[1].
-      add_substage('in progress', limit=2).
-        substages[1].add_card('E').add_card('Y')
+      add_substage('in progress', limit=3).
+        substages[1].add_card('Q').add_card('Y').add_card('W')
   kanban.
     stages[1].
       add_substage('done!').
-        substages[2].add_card('W').add_card('G')
+        substages[2]
 
   kanban.add_stage('deployment', limit=3).stages[2].add_card('E')
-  kanban.add_stage('in production').stages[3].add_card('F')
+  kanban.add_stage('in production').stages[3].add_card('F').add_card('K').add_card('L').add_card('M').add_card('N')
 
   @table = KanbanToTable.render(kanban)
   erb :index
