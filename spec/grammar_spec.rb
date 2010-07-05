65902a04a495c40df24f21e5dc121787718101c0
Rodrigo Yoshima <rodrigoy@gmail.com>
2010-07-05 01:34:53 -0300

    evolving the grammar to substages

--- spec/grammar_spec.rb
+++ spec/grammar_spec.rb
@@ -5,11 +5,17 @@ describe KanbanDSL do
 
   it "should render Stages with Cards" do
     parser = KanbanDSLParser.new()
-    sel = parser.parse("selected(A,B,C)") ## it should return the syntactical tree, but returns nil
+    sel = parser.parse("selected(A,B,C);" + 
+      "development[in progress(D,E)];")
 
     sel.should_not be_nil
-    sel.respond_to?(:stories).should be_true
-    parser.failure_reason.should be_nil
+    puts sel
+    ## sel.respond_to?(:cards).should be_true
+    sel.elements.each do |x|
+      puts x.text_value
+      x.elements.each {|y| puts "  " + y.text_value} if x.elements
+    end
+    ## parser.failure_reason.should be_nil
 
   end
 end
