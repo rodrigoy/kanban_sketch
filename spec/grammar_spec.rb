076522aea332b74b88964c66a0ec1eefb895563a
Rodrigo Yoshima <rodrigoy@gmail.com>
2010-07-05 02:12:30 -0300

    evolving the grammar to limits

--- spec/grammar_spec.rb
+++ spec/grammar_spec.rb
@@ -5,12 +5,11 @@ describe KanbanDSL do
 
   it "should render Stages with Cards" do
     parser = KanbanDSLParser.new()
-    sel = parser.parse("selected(A,B,C);" + 
-      "development[in progress(D,E)];")
+    sel = parser.parse("selected:2(A,B,C);" + 
+      "development:5[in progress:2(D,E)][done()];" +
+      "deploy(F,G,H,I);")
 
     sel.should_not be_nil
-    puts sel
-    ## sel.respond_to?(:cards).should be_true
     sel.elements.each do |x|
       puts x.text_value
       x.elements.each {|y| puts "  " + y.text_value} if x.elements
